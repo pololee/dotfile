729ac3d611eacd9c4a594dc1cf0bbf55a04ba662
Polo Lee <github@pololee.com>
2026-08-02 12:06:02 -0700

    remove Chrome vertical-tabs automation (#12)

--- packages/hammerspoon/.hammerspoon/init.lua
+++ packages/hammerspoon/.hammerspoon/init.lua
@@ -5,6 +5,5 @@ require("modules.mouse_highlight").start(config.hyper)
 require("modules.window_hints").start(config.hyper)
 require("modules.app_hotkeys").start(config.hyper)
 require("modules.scroll_direction").start()
-require("modules.chrome_vertical_tabs").start(config.chromeVerticalTabs)
 
 hs.alert.show("Config loaded")
